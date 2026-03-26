99df1b4f3fb94ed9e5d43b8e9840602bbabaeab9
Kundan Karn <kundankarn007@gmail.com>
2026-03-27 02:53:05 +0530

    JVM basics: checking separate objects

---
+++ xx/src/main/java/com/java/practise/jvm/SeparateObjects.java
@@ -0,0 +1,14 @@
+package com.java.practise.jvm;
+
+public class SeparateObjects {
+
+    public static void main(String[] args) {
+        Runnable runnable1 = new MyRunnable();
+        Runnable runnable2 = new MyRunnable();
+        Thread thread1 = new Thread(runnable1, "Thread-1");
+        Thread thread2 = new Thread(runnable2, "Thread-2");
+        thread1.start();
+        thread2.start();
+    }
+
+}
